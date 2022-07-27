0cb924c613d820bacc4ba0d9cc1fea3117e77df5
KieenVux <40339436+KieenVux@users.noreply.github.com>
2022-07-27 09:28:55 +0700

    add fullname

--- test/main.go
+++ test/main.go
@@ -4,5 +4,8 @@ import "fmt"
 
 func main() {
 	var a = "file main.go"
+	var firstName = "Kien"
+	var lastName = "Vu"
+	fmt.Printf("FullName: %s %s", firstName, lastName)
 	fmt.Printf("This is: %s", a)
 }
